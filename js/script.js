247423d1e5bd9712b604d28678f2c9c17e4f2170
Althea Nicole F. Gammad <angammad23108693@student.dmmmsu.edu.ph>
2026-06-30 20:11:40 +0800

    Convert to static website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-
+document.addEventListener("DOMContentLoaded", () => {
     console.log("Personal Profile Website Loaded");
+    const tags = document.querySelectorAll(".tag");
 
-
+    tags.forEach(tag => {
+        tag.addEventListener("mouseenter", () => {
+            tag.style.cursor = "pointer";
+        });
+    });
 });
